39355dc669dae13f21cde599d1aaa410c30d2fbb
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-03-15 23:03:57 +0900

    Updated Class and Interface on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-const sayKimchiTaste = (name: string, age: number, taste: string): string => {
-  return `${name}, this is ${age} year(s), taste is ${taste}!`;
+class Kimchi {
+  public name: string;
+  public year: number;
+  public taste: string;
+  constructor(name: string, year: number, taste: string) {
+    this.name = name;
+    this.year = year;
+    this.taste = taste;
+  }
+}
+
+interface IKimchi {
+  name: string;
+  year: number;
+  taste: string;
+}
+
+const mullKimchi = new Kimchi("Mull Kimchi", 3, "Good");
+
+const sayKimchiTaste = (kimchi: Kimchi): string => {
+  return `${kimchi.name}, this is ${kimchi.year} year(s), taste is ${kimchi.taste}!`;
 };
 
-console.log(sayKimchiTaste("Kimchi", 3, "Good"));
+console.log(sayKimchiTaste(mullKimchi));
 
 export {};
