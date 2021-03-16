bb0c08b8d831e0ffca174f282c980527fc7c7b6e
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-03-16 18:44:21 +0900

    Finished All

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,107 @@
-class Kimchi {
-  public name: string;
-  public year: number;
-  public taste: string;
-  constructor(name: string, year: number, taste: string) {
-    this.name = name;
-    this.year = year;
-    this.taste = taste;
+import * as CryptoJS from "crypto-js";
+
+class Block {
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-interface IKimchi {
-  name: string;
-  year: number;
-  taste: string;
-}
+const genesisBlock: Block = new Block(
+  0,
+  "asdasjdhasld",
+  "",
+  "Kimchi Land",
+  123456
+);
 
-const mullKimchi = new Kimchi("Mull Kimchi", 3, "Good");
+let blockchain: Block[] = [genesisBlock];
 
-const sayKimchiTaste = (kimchi: Kimchi): string => {
-  return `${kimchi.name}, this is ${kimchi.year} year(s), taste is ${kimchi.taste}!`;
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  addBlock(newBlock);
+  return newBlock;
 };
 
-console.log(sayKimchiTaste(mullKimchi));
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
 
-export {};
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+createNewBlock("Mull Kimchi");
+createNewBlock("Baechu Kimchi");
+createNewBlock("Dongchimi");
+
+console.log(getBlockchain());
